f2e2ba46d53edda218051c849dc0cf79ef5fff54
bruno_furtado <brunoroberto009@gmail.com>
2025-04-02 18:31:34 -0300

    commit teste 2

---
+++ src/MInhaClasse.java
@@ -0,0 +1,8 @@
+public class MInhaClasse {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAA ");
+        System.out.println("teste porra ");
+
+    }
+
+}
